1dd600b58def1a84962607be262d9effd851ac3a
CaspervdHoek <casper.vdhoek@live.nl>
2015-09-21 16:03:33 +0200

    adapter afgemaakt

--- src/nl/saxion/tinglr/model/TumblrPost.java
+++ src/nl/saxion/tinglr/model/TumblrPost.java
@@ -1,5 +1,35 @@
 package nl.saxion.tinglr.model;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class TumblrPost {
+	
+	private User user;
+	private String text;
+	
+	
+	public TumblrPost(JSONObject tumblrPost){
+		try {
+			this.user = new User(tumblrPost.getJSONObject("user"));
+			this.text = tumblrPost.getString("text");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	// Constructor zonder JSON
+	public TumblrPost(User user, String text){
+		this.user = user;
+		this.text = text;
+	}
+	
+	public User getUser(){
+		return user;
+	}
+	
+	public String getText(){
+		return text;
+	}
 
 }
--- src/nl/saxion/tinglr/view/TumblrPostAdapter.java
+++ src/nl/saxion/tinglr/view/TumblrPostAdapter.java
@@ -3,15 +3,43 @@ package nl.saxion.tinglr.view;
 import java.util.ArrayList;
 import java.util.List;
 
+import nl.saxion.tinglr.R;
 import nl.saxion.tinglr.model.TumblrPost;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class TumblrPostAdapter extends ArrayAdapter<TumblrPost> {
+	
+	private LayoutInflater inflater;
+	private int resource;
 
 	public TumblrPostAdapter(Context context, int resource, ArrayList<TumblrPost> objects) {
 		super(context, resource, objects);
-		// TODO Auto-generated constructor stub
+		inflater = LayoutInflater.from(context);
+		this.resource = resource;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if(convertView == null){
+			convertView = inflater.inflate(resource, parent, false);
+		}
+		
+		TextView userName = (TextView) convertView.findViewById(R.id.userName);
+		TextView tumblrPostText = (TextView) convertView.findViewById(R.id.tumblrPostText);
+		ImageView profielfoto = (ImageView) convertView.findViewById(R.id.profielFoto);
+		
+		TumblrPost tumblrPost = getItem(position);
+		
+		userName.setText(tumblrPost.getUser().getUserName());
+		tumblrPostText.setText(tumblrPost.getText());
+		
+		return convertView;
 	}
 
 }
