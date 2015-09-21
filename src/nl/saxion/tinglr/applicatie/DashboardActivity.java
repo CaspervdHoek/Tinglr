e14f3bb9b6667395ca08c1914291b6af0223ff12
CaspervdHoek <casper.vdhoek@live.nl>
2015-09-21 16:26:27 +0200

    goed

--- src/nl/saxion/tinglr/applicatie/DashboardActivity.java
+++ src/nl/saxion/tinglr/applicatie/DashboardActivity.java
@@ -1,24 +1,28 @@
 package nl.saxion.tinglr.applicatie;
 
+import nl.saxion.tinglr.R;
+import nl.saxion.tinglr.model.Model;
+import nl.saxion.tinglr.view.TumblrPostAdapter;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class DashboardActivity extends Activity {
 
 	private ListView listViewTumblrPosts;
-	private TextView realName, userName, following, followers, tweetCount;
-	private MyApplication app;
+	private TinglrApplication app;
 	private Model model;
-	private TweetAdapter adapter;
-	private ImageView profilePhoto;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tumblrpost);
 		
+		app = (TinglrApplication) getBaseContext().getApplicationContext();
+		model = app.getModel();
 		
-		
+		listViewTumblrPosts = (ListView) findViewById(R.id.listViewTumblrPosts);
 	}
 }
